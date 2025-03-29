5c768a334fce1db11ceaf8d17e6a0d8cfc1c43e3
Kamil Z <01148822@pw.edu.pl>
2025-03-29 20:17:42 +0100

    usawienie zaleznosci portu w main

--- backend/core/main.py
+++ backend/core/main.py
@@ -1,4 +1,5 @@
 import uvicorn
-
+import os
 if __name__ == "__main__":
-    uvicorn.run("core.api.api:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("core.api.api:app", host="0.0.0.0", port=port, reload=True)
