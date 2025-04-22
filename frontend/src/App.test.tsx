bc7d08ed759b514e85474b0efa91bf0e79b09931
Kamil Z <01148822@pw.edu.pl>
2025-04-22 20:57:14 +0200

    [CI] TEST 2 ci pod front

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+jest.mock('tone', () => ({}));
 test('submit file button', () => {
   render(<App />);
   const linkElement = screen.getByText(/Upload/i);
