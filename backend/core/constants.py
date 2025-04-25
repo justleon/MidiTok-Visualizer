2d3836b83c9259e85aa3f85540e05924047b5ecd
akochani <adam.kochaniecki@gmail.com>
2025-04-25 23:49:46 +0200

    names nb_ to num_

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -9,13 +9,13 @@ EXAMPLE_MIDI_FILE_PATH2= os.path.join(DATA_DIR, "example2.mid")
 DEFAULT_TOKENIZER_PARAMS = {
     "pitch_range": (21, 109),
     "beat_res": {(0, 4): 8, (4, 12): 4},
-    "nb_velocities": 32,
+    "num_velocities": 32,
     "special_tokens": ["PAD", "BOS", "EOS", "MASK"],
     "use_chords": True,
     "use_rests": False,
     "use_tempos": True,
     "use_time_signatures": False,
     "use_programs": False,
-    "nb_tempos": 32,
+    "num_tempos": 32,
     "tempo_range": (40, 250),
 }
